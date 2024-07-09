ab54d349de460de6019bce076c97439047c0a4f5
Elia Noah Hölzel <43881773+Badetuch@users.noreply.github.com>
2024-07-09 14:11:27 +0200

    font

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-      <div className={inter.className}>
+      <div>
         {children}
       </div>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Header from "@/components/Header";
 import Body from "@/components/Body";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 
 const Page = () => {
   return (
-    <div>
+    <div className={inter.className} >
       <Header/>
       <Body/>
     </div>
