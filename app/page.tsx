e6079c621a36daf955b4cabafdfe8decad422f73
Elia Noah Hölzel <43881773+Badetuch@users.noreply.github.com>
2024-07-09 14:19:43 +0200

    font2

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
 import Header from "@/components/Header";
 import Body from "@/components/Body";
-import { Inter } from "next/font/google";
-const inter = Inter({ subsets: ["latin"] });
 
 const Page = () => {
   return (
-    <div className={inter.className} >
+    <div >
       <Header/>
       <Body/>
     </div>
