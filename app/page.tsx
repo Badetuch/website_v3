72918fe83c805b87c93615deaa64e6ea9e28f149
Elia Noah Hölzel <43881773+Badetuch@users.noreply.github.com>
2024-07-13 01:48:14 +0200

    added footer radient glow for upcoming changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Header from "@/components/Header";
 import Body from "@/components/Body";
+import Footer from "@/components/footer";
 
 const Page = () => {
   return (
     <div>
       <Header/>
       <Body/>
+      <Footer/>
     </div>
   );
 };
