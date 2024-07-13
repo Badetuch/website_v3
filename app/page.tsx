b833e91026c56b2616235e99eb876de9bd75e9cc
Elia Noah Hölzel <43881773+Badetuch@users.noreply.github.com>
2024-07-14 00:07:41 +0200

    added footer glow and background animation

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import Header from "@/components/Header";
 import Body from "@/components/Body";
 import Footer from "@/components/Footer";
+import { BackgroundBeams } from "@/components/ui/background-beams";
 
 const Page = () => {
   return (
     <div>
+      <BackgroundBeams/>
       <Header/>
       <Body/>
       <Footer/>
