6065f48189800e72cd05e6967e00a25cd8fa3a7d
Elia Noah Hölzel <43881773+Badetuch@users.noreply.github.com>
2024-07-13 20:37:44 +0200

    added footer base

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Header from "@/components/Header";
 import Body from "@/components/Body";
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer";
 
 const Page = () => {
   return (
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 h-5 w-full bg-white/[0.3] inset-x-0 rounded-full blur-2xl"></div>
+    <footer className="bg-[#05020D] rounded-lg shadow ">
+      <hr className=" border-[#3F3F3F] border-1" />
+        <div className="w-full max-w-screen-xl p-4 md:py-8">
+            <div className="sm:flex sm:items-center sm:justify-between">
+                <a className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                </a>
+                <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 ">
+                    <li>
+        
+                        <a href="#" className="text-[16px] font-regular hover:underline hover:text-white transition-colors duration-200 me-4 md:me-6">Links</a>
+                    </li>
+                    <li>
+                      
+                      <a href="#" className="text-[16px] font-regular hover:underline hover:text-white transition-colors duration-200 me-4 md:me-6">About Us</a>
+                    </li>
+                    <li>
+                      
+                        <a href="#" className="text-[16px] font-regular hover:underline hover:text-white transition-colors duration-200 me-4 md:me-6">Imprint</a>
+                    </li>
+                    <li>
+                        <a href="#" className="text-[16px] font-regular hover:underline hover:text-white transition-colors duration-200 me-4 md:me-6">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="#" className="text-[16px] font-regular hover:underline hover:text-white transition-colors duration-200 me-4 md:me-6">TeamSpeak Rules</a>
+                    </li>
+                    <li>
+                        <a href="#" className="text-[16px] font-regular hover:underline hover:text-white transition-colors duration-200 me-4 md:me-6">Status</a>
+                    </li>
+                </ul>
+            </div>
+            <span className="block text-sm text-gray-500 sm:text-center">© 2024 Badespeak. All Rights Reserved.</span>
+        </div>
+        {/* <div className=" h-3 w-28 bg-white/[1] inset-x-0 rounded-full blur-2xl"></div> Radient Glow */}
+    </footer>
+
+
   )
 }
 
