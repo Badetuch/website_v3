b22dd0bef442ba1e70bd5998080dccc7e0836cdc
Elia Noah Hölzel <43881773+Badetuch@users.noreply.github.com>
2024-07-16 15:58:44 +0200

    added page routing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -21,30 +21,34 @@ const Footer = () => {
               </div>
             </a>
           </div>
-          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+          <div className="grid grid-cols-2 gap-28 sm:gap-28 sm:grid-cols-2">
             <div>
-              <h2 className="mb-6 text-[20px] font-bold text-white ">Links</h2>
+              <h2 className="mb-2 text-[20px] font-bold text-white ">Links</h2>
               <ul className="text-[#686868] font-normal">
-                <li className="mb-4">
-                  <a href="https://flowbite.com/" className="hover:underline hover:text-white">Privacy Policy</a>
+                <li className="mb-1">
+                  <a href="https://Badespeak.net/ts3-rules" className="hover:underline hover:text-white">TeamSpeak Rules</a>
                 </li>
                 <li>
-                  <a href="https://tailwindcss.com/" className="hover:underline hover:text-white">TeamSpeak Rules</a>
+                  <a href="https://status.badespeak.net" className="hover:underline hover:text-white">Status</a>
                 </li>
               </ul>
             </div>
             <div>
-              <h2 className="mb-6 text-[20px] font-bold text-white ">About Us</h2>
+              <h2 className="mb-2 text-[20px] font-bold text-white ">About Us</h2>
               <ul className="text-[#686868] font-normal">
-                <li className="mb-4">
+                <li className="mb-1">
                   <a href="https://github.com/Badespeak" className="hover:underline hover:text-white">Imprint</a>
+                </li>
+                <li>
+                  <a href="https://Badespeak.net/privacy-policy" className="hover:underline hover:text-white">Privacy Policy</a>
                 </li>
               </ul>
             </div>
           </div>
         </div>
         <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 Badespeak</span>
+          <span className="font-light text-[14px] sm:text-center text-[#686868]">© 2024 Badespeak</span>
+          <span className="font-light text-[14px]">A project by <a href="html://x.com/badespeakowner" className="text-[#0070F3]">@badespeakowner</a></span>
           <div className="flex mt-4 sm:justify-center sm:mt-0">
             <a href="https://x.com/badespeak" target='_blank' className="text-gray-500 hover:text-white ms-5">
               <IconContext.Provider value={{ color:"#686868", className: "contactIcon" }}>
