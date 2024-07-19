2758f8a6291fa16c72782f16fb2bf92a79da568f
Elia Noah Hölzel <43881773+Badetuch@users.noreply.github.com>
2024-07-20 00:36:28 +0200

    added speedinsights and opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,24 @@ import "./globals.css";
 import { BackgroundBeams } from "@/components/ui/background-beams";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Badespeak.net - Public TeamSpeak Server",
-  description: "Badespeak.net - Get your free channel today!",
+  openGraph: {
+    title: "Badespeak.net - Public TeamSpeak Server",
+    description: "Badespeak.net - Get your free channel today!",
+    url: 'https://Badespeak.net',
+    siteName: 'Badespeak.net',
+    images: [
+      {
+        url: '/img/LOGO.srv',
+        width: 500,
+        height: 500,
+      },
+    ]
+  }
 };
 
 export default function RootLayout({
@@ -19,6 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <SpeedInsights/>
       <body className={inter.className}>
       <div>
       <BackgroundBeams/>
